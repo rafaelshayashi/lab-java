e94892ba41a3caee5ef87906ff970b0e1b06a73f
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2025-11-20 13:42:31 -0300

    feat: add applications

---
+++ applications/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,61 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private Integer paginas;
+	
+	@ElementCollection
+	private List<Precos> precos;
+	
+
+	public Integer getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Integer getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+	public List<Precos> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Precos> precos) {
+		this.precos = precos;
+	}
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas
+				+ ", precos=" + precos + "]";
+	}
+
+	
+
+
+}
